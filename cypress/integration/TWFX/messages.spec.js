b352b3d1fd8431a284f8b317e3d08b023c760623
felixpro <pujols@gmail.com>
2021-08-25 17:45:43 -0400

    New toDo list tests

--- cypress/integration/TWFX/messages.spec.js
+++ cypress/integration/TWFX/messages.spec.js
@@ -1,7 +1,7 @@
 
 describe('adding and deleting a todo list', () => {
     beforeEach(() => {
-      cy.visit('https://app.webfx.com/projects/14993480/posts', {
+      cy.visit('/projects/14993480/posts', {
         timeout: 20000,
       })
       cy.get("#email").type("fpujols@webfx.com");
@@ -9,23 +9,26 @@ describe('adding and deleting a todo list', () => {
       cy.get('form').submit()
     })
   
-  
      it("Create Message", () => {
-        cy.contains('span', 'post a new message').click();
+        cy.percySnapshot("TWFX - Before Create Message");
+        cy.contains('post a new message').click();
         cy.get('#title').type("Admin Message #1");
         cy.get('.ql-editor').type("Description message");
         cy.get('.Button-module_primary_zic6V').click();
         cy.get('.Button-module_danger_eM7K_').click();
         cy.contains('div', 'Admin Message #1');
+        cy.percySnapshot("TWFX - After Create Message");
     }) 
 
     it("Edit Message - set to private", () => {
+        cy.percySnapshot("TWFX - Before Edit Message");
         cy.contains('Admin Message #1').click();
         cy.contains('a', 'Edit this message').click();
         cy.get('#title').type(' - Edited');
         cy.get('.ql-editor').type(' - Edited');
         cy.get('#private').check();
         cy.contains('button', 'Save changes').click();
+        cy.percySnapshot("TWFX - After Edit Message");
         cy.contains('div', 'Admin Message #1 - Edited');
         cy.contains('span','only people from your company can see this ');
     }) 
@@ -34,16 +37,25 @@ describe('adding and deleting a todo list', () => {
         cy.contains('div', 'Admin Message #1 - Edited').click();
         cy.get('.ql-editor').type('Admin comment - Cy Test');
         cy.contains('span','Post a comment').click();
+        cy.contains('Admin comment - Cy Test')
+        cy.percySnapshot("TWFX - After comment Message");
     });
+
+    it("Delete a comment", () => {
+        cy.contains('div', 'Admin Message #1 - Edited').click();
+        cy.get('.CommentItemHeader-module_delete_3zTY4 > .Icon-module_materialIcons_3f-C2').click();
+        cy.get('.Button-module_danger_eM7K_ > .Button-module_contentWrap_1lSJB > .Button-module_text_2AXJY').click();
+        cy.contains('div', 'Admin comment - Cy Test').should('not.exist');
+        cy.percySnapshot("TWFX - After Deleting a comment");
+      });
 
    it("Delete message", () => {
        cy.contains('div', 'Admin Message #1 - Edited').click();
        cy.get('.MessageToolbar-module_items_16IKg > :nth-child(3) > div').click();
        cy.get('.Button-module_danger_eM7K_ > .Button-module_contentWrap_1lSJB > .Button-module_text_2AXJY').click();
        cy.contains('div', 'Admin Message #1 - Edited').should('not.exist');
+       cy.percySnapshot("TWFX - After Deleting a message");
    }) 
-
-
     
   }) 
   
--- cypress/integration/TWFX/todoList.spec.js
+++ cypress/integration/TWFX/todoList.spec.js
@@ -1,14 +1,13 @@
 
 describe('adding and deleting a todo list', () => {
   beforeEach(() => {
-    cy.visit('https://app.webfx.com/projects/14993480/todo-lists', {
+    cy.visit('/projects/14993480/todo-lists', {
       timeout: 20000,
     })
     cy.get("#email").type("fpujols@webfx.com");
     cy.get("#password").type("PA1SEO20Conv20?!");
     cy.get('form').submit()
   })
-
 
   it("Create ToDo-list", () => {
     cy.get('.col-lg-3 > .Button-module_btn_3vLCr').click();
@@ -17,22 +16,54 @@ describe('adding and deleting a todo list', () => {
     cy.get('.Modal-module_footerFinishCell_AmcJf > .Button-module_primary_1NBBI').click();
     cy.contains('div', 'Create ToDo-list - Name')
     cy.contains('Create ToDo-list - Name').click()
+    cy.percySnapshot("TWFX - Create ToDo-list");
   })
 
   it("Create Todo", () => {
     cy.contains('Create ToDo-list - Name').click()
-    /* ==== Generated with Cypress Studio ==== */
     cy.get('.Button-module_text_1TOez').click();
-    cy.get('#content').click();
+    cy.get('#content').type("Todo#1");
     cy.get('.Button-module_primary_1NBBI > .Button-module_contentWrap_j6nhS > .Button-module_text_1TOez').click();
-    cy.get('.Button-module_borderless_3DxjV > .Button-module_contentWrap_j6nhS > .Button-module_text_1TOez').click();
-    /* ==== End Cypress Studio ==== */
+    cy.get('.Todo-module_content_2BGcA > div').should('have.text', 'Todo#1');
   })
-     
 
- /* it('Delete ToDo-list', function() {
-    cy.contains('h3', 'Create ToDo-list - Name').click()
-    cy.get('.Button-module_danger-outline_2G3tf > .Button-module_contentWrap_j6nhS > .Button-module_icon_3Rux2').click({ force: true })
+  it("Edit Todo", () => {
+    cy.get('div.TableCell-module_column_3v9bX.TableCell-module_grow_2Tbm7.d-flex.align-items-center.text-left > div.OverlayActions-module_extraSpace_1wbM0.Todo-module_overlay_1g_el > div.OverlayActions-module_overlay_3s__y > button.Button-module_btn_3vLCr.Button-module_primary-outline_2PuTS.Button-module_borderless_3DxjV.Button-module_btnSm_1zK9l.OverlayActions-module_overlayButton_3wkLg').click({ force: true });
+    cy.get('#content').click().type('- edited');
+    cy.get('.TodoForm-module_footer_3z3QX > .Button-module_primary_1NBBI').click();
+    cy.percySnapshot("TWFX - Todo edited");
+  })
+
+  it("Add due date and priority", () => {
+    cy.get('.Todo-module_dateIcon_2qDeU').click();
+    cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(1) > .DayPickerNavigation_select').select('0');
+    cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(2) > .DayPickerNavigation_select').select('2020');
+    cy.get('[aria-label="Thursday, January 2, 2020"]').click();
+    cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
+    cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
+    cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
+    cy.percySnapshot("TWFX - Adding due date and priority to ToDos");
+  })
+
+  it("Add responsable", () => {
+    cy.percySnapshot("TWFX - Add responsable");
+    cy.get('.TodoAssigneeView-module_icon_iLH_a').click();
+    cy.get('.css-2n3929-singleValue > .SingleValue-module_container_1Fl9F').click();
+    cy.get('#react-select-4-input').clear();
+    cy.get('#react-select-4-input').type('felix test');
+    cy.get('.Option-module_label_1i6B3').click();
+  })
+
+
+ it("Delete Todo", () => {
+    cy.get('div.TableCell-module_column_3v9bX.TableCell-module_grow_2Tbm7.d-flex.align-items-center.text-left > div.OverlayActions-module_extraSpace_1wbM0.Todo-module_overlay_1g_el > div.OverlayActions-module_overlay_3s__y > button.Button-module_btn_3vLCr.Button-module_danger-outline_2G3tf.Button-module_borderless_3DxjV.Button-module_btnSm_1zK9l.OverlayActions-module_overlayButton_3wkLg').click({ force: true });
     cy.get('.Button-module_danger_1Ygh8').click();
-  }); */
+ })
+
+ it("Delete ToDoList", () => {
+   cy.percySnapshot("TWFX - Delete ToDoList");
+   cy.get('.Button-module_danger-outline_2G3tf > .Button-module_contentWrap_j6nhS > .Button-module_icon_3Rux2').click({ force: true });
+   cy.get('.Button-module_danger_1Ygh8 > .Button-module_contentWrap_j6nhS > .Button-module_text_1TOez').click();
+ })
+     
 })
