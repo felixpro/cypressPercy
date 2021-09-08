ffafa584766b03252fec1f53e61e6690ae836997
felixpro <pujols@gmail.com>
2021-09-08 15:32:07 -0400

    Complete message automate tests

--- cypress/integration/TWFX/messages.spec.js
+++ cypress/integration/TWFX/messages.spec.js
@@ -33,6 +33,22 @@ describe('Messages', () => {
         cy.contains('span','only people from your company can see this ');
     });
 
+  it("Should add a category and milestone to message", () => {
+        cy.contains('Admin Message #1').click();
+        cy.contains('a', 'Edit this message').click();
+        cy.get('#category-input').click();
+        cy.contains('Calls').click();
+        cy.get('#milestone-msg').click();
+        cy.contains('Cypress Automation').click();
+        cy.contains('button', 'Save changes').click();
+        cy.contains('Admin Message #1').click();
+        cy.contains('a', 'Edit this message').click();
+        cy.contains('Calls');
+        cy.contains('Cypress Automation');
+        cy.percySnapshot("TWFX - After adding category and milestone to Message");
+    });
+
+
     it("Should comment a message", () => {
         cy.contains('div', 'Admin Message #1 - Edited').click();
         cy.get('.ql-editor').type('Admin comment - Cy Test');
@@ -49,12 +65,33 @@ describe('Messages', () => {
         cy.percySnapshot("TWFX - After Deleting a comment");
       });
 
-   it("Should delete a message", () => {
+    it("Should create a Message with template and subscriber", () => {
+        cy.percySnapshot("TWFX - Before Create Message with template");
+        cy.contains('post a new message').click();
+        cy.get('#title').type("Admin Message from template #2");
+        cy.get('#template-msg').click()
+        cy.contains('Onboarding Call Follow Up').click();
+        cy.wait(5000);
+        cy.contains('div', 'Subscribe people to receive email notifications').click()
+        cy.contains('Felix/Client#3 Pujols').click();
+        cy.percySnapshot("TWFX - Message input with template and subscriber");
+        cy.get('.submit_msg_btn').click();
+        cy.contains('Admin Message from template #2').click();
+        cy.contains('div', 'Admin Message from template #2');
+        cy.percySnapshot("TWFX - After Create Message with template");
+    });
+
+   it("Should delete messages", () => {
        cy.contains('div', 'Admin Message #1 - Edited').click();
        cy.get('#delete-msg').click();
        cy.get('.confirm_btn').click();
        cy.contains('div', 'Admin Message #1 - Edited').should('not.exist');
-       cy.percySnapshot("TWFX - After Deleting a message");
+       // Delete second message
+       cy.contains('div', 'Admin Message from template #2').click();
+       cy.get('#delete-msg').click();
+       cy.get('.confirm_btn').click();
+       cy.contains('div', 'Admin Message from template #2').should('not.exist');
+       cy.percySnapshot("TWFX - After Deleting messages");
    });
     
   });
