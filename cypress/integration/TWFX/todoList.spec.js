3015c5487bdca95679047cd9ef44d9d960d5eaaa
felixpro <pujols@gmail.com>
2021-09-10 17:49:16 -0400

    TodoList modification

--- cypress/integration/TWFX/todoList.spec.js
+++ cypress/integration/TWFX/todoList.spec.js
@@ -9,7 +9,7 @@ describe('adding and deleting a todo list', () => {
     cy.get('form').submit()
   })
 
-  it("Create ToDo-list", () => {
+  it("Should create a ToDo-list", () => {
     cy.get('.new-todoList-btn').click();
     cy.get('#name').type("Admin - new ToDo-list")
     cy.get('#description').type("Create ToDo-list - description")
@@ -19,52 +19,71 @@ describe('adding and deleting a todo list', () => {
     cy.percySnapshot("TWFX - Create ToDo-list");
   })
 
-  it("Create Todo", () => {
-    cy.contains('Admin - new ToDo').click()
-    cy.get('.new-todoBtn').click();
-    cy.get('#content').type("Todo#1");
-    cy.get('.saveTodo-btn').click();
-    cy.get('.done-btn').click();
-    cy.get('.Todo-module_content_2BGcA > div').should('have.text', 'Todo#1');
-  })
+//   it("Should create a Todo", () => {
+//     cy.contains('Admin - new ToDo').click()
+//     cy.get('.new-todoBtn').click();
+//     cy.get('#content').type("Todo#1");
+//     cy.get('.saveTodo-btn').click();
+//     cy.get('.done-btn').click();
+//     cy.contains('Todo#1');
+//     cy.percySnapshot("TWFX - Create ToDo");
+//   })
 
-//   it("Edit Todo", () => {
-//     cy.get('div.TableCell-module_column_3v9bX.TableCell-module_grow_2Tbm7.d-flex.align-items-center.text-left > div.OverlayActions-module_extraSpace_1wbM0.Todo-module_overlay_1g_el > div.OverlayActions-module_overlay_3s__y > button.Button-module_btn_3vLCr.Button-module_primary-outline_2PuTS.Button-module_borderless_3DxjV.Button-module_btnSm_1zK9l.OverlayActions-module_overlayButton_3wkLg').click({ force: true });
-//     cy.get('#content').click().type('- edited');
-//     cy.get('.TodoForm-module_footer_3z3QX > .Button-module_primary_1NBBI').click();
+//   it("Should edit a Todo", () => {
+//     cy.get('.todo-overlay .edit-todoBtn').click({ force: true });
+//     cy.get('#content').click().type(' - edited');
+//     cy.get('.saveTodo-btn').click();
 //     cy.percySnapshot("TWFX - Todo edited");
 //   })
 
-//   it("Add due date and priority", () => {
-//     cy.get('.Todo-module_dateIcon_2qDeU').click();
-//     cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(1) > .DayPickerNavigation_select').select('0');
-//     cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(2) > .DayPickerNavigation_select').select('2020');
-//     cy.get('[aria-label="Thursday, January 2, 2020"]').click();
-//     cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
-//     cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
-//     cy.get(':nth-child(3) > .TodoPriority-module_priorityController_xq7eD').click();
+//   it("Should Add a due date and priority", () => {
+//     cy.get('.todo-container .todoPriority-btn').click();
+//     cy.get('.todo-container .DateInput').click();
+//     cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(1) > .DayPickerNavigation_select').select('10');
+//     cy.get(':nth-child(2) > .CalendarMonth > .CalendarMonth_caption > .DayPickerNavigation_selects > :nth-child(2) > .DayPickerNavigation_select').select('2000');
+//     cy.get('[aria-label="Wednesday, November 8, 2000"]').click();
 //     cy.percySnapshot("TWFX - Adding due date and priority to ToDos");
 //   })
 
-//   it("Add responsable", () => {
+//   it("Should Add a responsable", () => {
 //     cy.percySnapshot("TWFX - Add responsable");
-//     cy.get('.TodoAssigneeView-module_icon_iLH_a').click();
+//     cy.get('.assignee-btn').click();
 //     cy.get('.css-2n3929-singleValue > .SingleValue-module_container_1Fl9F').click();
 //     cy.get('#react-select-4-input').clear();
 //     cy.get('#react-select-4-input').type('felix test');
 //     cy.get('.Option-module_label_1i6B3').click();
 //   })
 
+//   it("Should edit a Todo", () => {
+//     cy.get('.todo-overlay .edit-todoBtn').click({ force: true });
+//     cy.get('#content').click().type(' - edited');
+//     cy.get('.saveTodo-btn').click();
+//     cy.percySnapshot("TWFX - Todo edited");
+//   })
 
-//  it("Delete Todo", () => {
-//     cy.get('div.TableCell-module_column_3v9bX.TableCell-module_grow_2Tbm7.d-flex.align-items-center.text-left > div.OverlayActions-module_extraSpace_1wbM0.Todo-module_overlay_1g_el > div.OverlayActions-module_overlay_3s__y > button.Button-module_btn_3vLCr.Button-module_danger-outline_2G3tf.Button-module_borderless_3DxjV.Button-module_btnSm_1zK9l.OverlayActions-module_overlayButton_3wkLg').click({ force: true });
-//     cy.get('.Button-module_danger_1Ygh8').click();
+
+
+
+//  it("Should Delete a Todo", () => {
+//     cy.get('.todo-container .delete-todoBtn').click({ force: true });
+//     cy.contains('button', 'Yes').click();
+//     cy.contains('div', 'Todo#1 - edited').should('not.exist');
 //  })
 
-//  it("Delete ToDoList", () => {
+//  it("Should edit a ToDo-list", () => {
+//   cy.get('.todoList-header .edit-todoBtn').click({ force: true });
+//   cy.get('#name').type(" - Edited")
+//   cy.get('#description').type(" - Edited")
+//   cy.contains('SAVE CHANGES').click();
+//   cy.contains('div', 'Admin - new ToDo-list - Edited')
+//   cy.percySnapshot("TWFX - Create ToDo-list");
+// })
+
+
+//  it("Should Delete ToDoList", () => {
 //    cy.percySnapshot("TWFX - Delete ToDoList");
-//    cy.get('.Button-module_danger-outline_2G3tf > .Button-module_contentWrap_j6nhS > .Button-module_icon_3Rux2').click({ force: true });
-//    cy.get('.Button-module_danger_1Ygh8 > .Button-module_contentWrap_j6nhS > .Button-module_text_1TOez').click();
+//    cy.get('.todoList-header .delete-todoBtn').click({ force: true });
+//    cy.contains('button', 'Yes').click();
 //  })
      
 })
